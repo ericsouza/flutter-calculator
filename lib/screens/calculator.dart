ac7637e2c6808d5b55caacde63ecd5e87181050d
Eric Souza <ericsouza0801@gmail.com>
2020-01-29 22:19:59 -0300

    init commit

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: <Widget> [
+          Text('Display'),
+          Text('Keyboard'),
+        ],
+      ),
+    );
+  }
+}
